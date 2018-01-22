629cd50b10b4981ecc483f789d4f60a63041bd1b
Sascha_T <info@sascha-t.de>
2018-01-22 14:02:48 +0100

    Update main.js

--- JS/__test__/main.js
+++ JS/__test__/main.js
@@ -1,15 +1,5 @@
 var cryptoX = require(__testDir + "/../main.js")
-try {
 	//if(process.env.TRAVIS === "true") {
-		// Import Eye.JS
-		eye.describe("Utility Test", () => {
-		    eye.test("Randomizer Test", "node",
-			   $ => $(cryptoX.random(1, 10)).is('number')
-			)	
-		    eye.test("Key Chain Test", "node",
-			   $ => $(Array.isArray(cryptoX.keychainGen(5))).Equal(true)
-			)
-		})
 		var EncryptionText = "Hello";
 		var encryptedText = cryptoX.encrypt(EncryptionText, 10)
 		var expectedText = "720:1010:1080:1080:1110"
@@ -26,6 +16,4 @@ try {
 	/*} else {
 		console.log("Hello!")
 	}*/
-} catch (ex) {
-	console.log(ex.trace)
-}
+
